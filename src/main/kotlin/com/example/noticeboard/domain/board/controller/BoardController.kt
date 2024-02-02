c4e47f0d6b31e478264710f614aa62ae3205d411
siadeu87 <woxingya098@gmail.com>
2024-02-02 17:50:19 +0900

    디펙토리 주소 변경

--- src/main/kotlin/com/example/noticeboard/domain/board/controller/BoardController.kt
+++ src/main/kotlin/com/example/noticeboard/domain/board/controller/BoardController.kt
@@ -11,7 +11,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/board")
+@RequestMapping("/boards")
 class BoardController(
     private val boardService: BoardService
 ) {
--- src/main/kotlin/com/example/noticeboard/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/example/noticeboard/domain/comment/controller/CommentController.kt
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/board/{boardId}/comment")
+@RequestMapping("/boards/{boardId}/comments")
 class CommentController(
     private val commentService: CommentService
 ) {
